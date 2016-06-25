da5c8343491e49bf8c44ad1ae8ad60abfca87c60
aleksandarbos <aleksandarbos.ns@gmail.com>
2016-06-25 15:05:23 +0200

    Implemented sending business requets to receipt-yourself app

--- app/controllers/HttpInvoker.java
+++ app/controllers/HttpInvoker.java
@@ -1,8 +1,11 @@
 package controllers;
 
-import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 import static javax.ws.rs.core.HttpHeaders.USER_AGENT;
 
@@ -11,16 +14,16 @@ import static javax.ws.rs.core.HttpHeaders.USER_AGENT;
  */
 public class HttpInvoker {
 
-    public static void sendPOST(String url, String data) throws Exception {
+    public static void sendPOST(String url, HashMap<String, String> map) throws Exception {
         URL obj = new URL(url);
-        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
         //add reuqest header
         con.setRequestMethod("POST");
         con.setRequestProperty("User-Agent", USER_AGENT);
         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-        String urlParameters = "data=" + data;
+        String urlParameters = "basicInfo=" + map.get("basicInfo")+"&contactInfo=" + map.get("contactInfo");
 
         // Send post request
         con.setDoOutput(true);
@@ -28,5 +31,19 @@ public class HttpInvoker {
         wr.writeBytes(urlParameters);
         wr.flush();
         wr.close();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
     }
 }
